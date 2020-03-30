e610e62c7fc77118f7b6364be27db56c88092d24
“metky” <vostrodmitry@outlook.com>
2020-03-30 11:39:37 +0300

    lesson 2.1 step 11

--- init.sh
+++ init.sh
@@ -1,6 +1,15 @@
 # !bin/bash
 
+# pre_init
+sudo apt update;
+sudo apt install python3.5;
+sudo rm /usr/bin/python3;
+sudo ln -s /usr/bin/python3.5 /usr/bin/python3; 
+sudo pip3 install gunicorn;
+sudo pip3 install django==2.1;
+
+# init
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo gunicorn -c hello.py hello:wsgi_application
+sudo gunicorn -c /home/box/web/etc/hello.py hello:wsgi_application &
+sudo gunicorn -c /home/box/web/etc/django_conf.py ask.wsgi:application
